c84095ed69c94a4b047128dda10d582c98d85e54
Nicolas Salomon <nicolassalomon96@gmail.com>
2024-01-10 18:53:41 -0300

    Train started

---
+++ VisDrone/utils/annotation_formatter.py
@@ -0,0 +1,41 @@
+#Convert annotation format into YOLOv8 format
+
+import os
+import sys
+import imageio.v2 as io
+
+dataset_path = r'..\dataset\Task1_Image_detection'
+train_labels_path = os.path.join(dataset_path, rf'train\labels_original')
+valid_labels_path = os.path.join(dataset_path, rf'valid\labels_original')
+test_labels_path = os.path.join(dataset_path, rf'test\labels_original')
+train_images_path = os.path.join(dataset_path, rf'train\images')
+valid_images_path = os.path.join(dataset_path, rf'valid\images')
+test_images_path = os.path.join(dataset_path, rf'test\images')
+
+def generate_new_labels(label_path, new_label_file_path, images_path):
+    label_files = os.listdir(label_path)
+    for label_file in label_files:
+        image = io.imread(os.path.join(images_path, f'{label_file.split(sep=".")[0]}.jpg'))
+        image_height, image_width = image.shape[0], image.shape[1]
+        with open(os.path.join(label_path, label_file), 'r') as f:
+            labels = [label.strip() for label in f.readlines()]
+            for annot in (labels):
+                #bbox_left, bbox_top, bbox_width, bbox_height, score, object_category, truncation, occlusion = annot.split(sep=',') #String format
+                bbox_left, bbox_top, bbox_width, bbox_height, score, object_category, truncation, occlusion = [int(x) for x in annot.split(sep=',')] #Int format
+                if (object_category == 0 or object_category == 11): #Filter the ignore and another class
+                    continue
+                with open(os.path.join(new_label_file_path, label_file), 'a+') as archivo:
+                    archivo.write(f'{object_category-1} {(bbox_left + bbox_width//2)/image_width} {(bbox_top + bbox_height//2)/image_height} {bbox_width/image_width} {bbox_height/image_height}\n')
+
+def main():
+    new_train_label_file_path = os.path.join(dataset_path, rf'train\labels')
+    new_valid_label_file_path = os.path.join(dataset_path, rf'valid\labels')
+    new_test_label_file_path = os.path.join(dataset_path, rf'test\labels')
+
+    generate_new_labels(train_labels_path, new_train_label_file_path, train_images_path)
+    generate_new_labels(valid_labels_path, new_valid_label_file_path, valid_images_path)
+    generate_new_labels(test_labels_path, new_test_label_file_path, test_images_path)
+
+
+if __name__ == '__main__':
+    main()
--- VisDrone/utils/utils_funcitons.py
+++ VisDrone/utils/utils_funcitons.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import imageio.v2 as io
 import cv2
 
-classes = ['ignore', 'pedestrian', 'people', 'bicycle', 'car', 'van', 'truck', 'tricycle', 'awning-tricycle', 'bus', 'motor', 'others']
+#classes = ['ignore', 'pedestrian', 'people', 'bicycle', 'car', 'van', 'truck', 'tricycle', 'awning-tricycle', 'bus', 'motor', 'others']
+classes = ['pedestrian', 'people', 'bicycle', 'car', 'van', 'truck', 'tricycle', 'awning-tricycle', 'bus', 'motor']
 
 def read_labels_file(path):
     with open(path) as f:
@@ -21,13 +22,32 @@ def display_image(image, figsize=(10,10)):
     plt.title('Example')
     plt.show()
 
-def display_image_w_annotations(image, annotation_list, max_annot=1000):
+def display_image_w_annotations_original(image, annotation_list, max_annot=1000):
+    #Original dataset annotation format: bbox_left, bbox_top, bbox_width, bbox_height, score, object_category, truncation, occlusion
+    image_cp = image.copy()
     for i, annot in enumerate(annotation_list):
         if i == max_annot:          
             break
         #bbox_left, bbox_top, bbox_width, bbox_height, score, object_category, truncation, occlusion = annot.split(sep=',') #String format
         bbox_left, bbox_top, bbox_width, bbox_height, score, object_category, truncation, occlusion = [int(x) for x in annot.split(sep=',')] #Int format
-        image = cv2.rectangle(image, (bbox_left, bbox_top), (bbox_left + bbox_width, bbox_top + bbox_height), (0,0,255), 2)
-        cv2.putText(image, classes[object_category], (bbox_left, bbox_top - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+        image_cp = cv2.rectangle(image_cp, (bbox_left, bbox_top), (bbox_left + bbox_width, bbox_top + bbox_height), (0,0,255), 2)
+        cv2.putText(image_cp, classes[object_category], (bbox_left, bbox_top - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
     
-    display_image(image)
+    display_image(image_cp)
+
+def display_image_w_annotations_YOLO(image, annotation_list, max_annot=1000):
+    #YOLO dataset annotation: class_id, center_x, center_y, width, height
+    image_cp = image.copy()
+    image_height, image_width = image.shape[0], image.shape[1]
+    for i, annot in enumerate(annotation_list):
+        if i == max_annot:          
+            break
+        class_id, center_x, center_y, width, height = [float(x) for x in annot.split(sep=' ')] #Int format
+        center_x = int(center_x * image_width)
+        center_y = int(center_y * image_height)
+        width = int(width * image_width)
+        height = int(height * image_height)
+        image_cp = cv2.rectangle(image_cp, (center_x-width//2, center_y-height//2), (center_x+width//2, center_y+height//2), (0,0,255), 2)
+        cv2.putText(image_cp, classes[int(class_id)], (center_x-width//2, center_y-height//2 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+        
+    display_image(image_cp)
