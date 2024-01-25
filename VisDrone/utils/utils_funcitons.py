cd47e40fdab5cd2af262355169c959487941f657
Nicolas Salomon <nicolassalomon96@gmail.com>
2024-01-24 22:35:14 -0300

    Utils functions updated

--- VisDrone/utils/utils_funcitons.py
+++ VisDrone/utils/utils_funcitons.py
@@ -51,3 +51,63 @@ def display_image_w_annotations_YOLO(image, annotation_list, max_annot=1000):
         cv2.putText(image_cp, classes[int(class_id)], (center_x-width//2, center_y-height//2 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
         
     display_image(image_cp)
+
+def predict(model, image, mode='detection', plot=False, conf_thresh=0.5, objects=None):
+    #conf_thresh = confidence threshold for object detection
+    #objects = LIST of objects to detect
+
+    number_class_list = []
+    if objects!=None:
+        if objects!=['all']:
+            for object in objects: 
+                number_class_list.append(classes.index(object))
+        elif objects == ['all']:
+            number_class_list = list(range(len(classes)))
+    
+    results = model.predict(image, verbose=False, conf=conf_thresh, classes=number_class_list)
+    result_boxes = []
+    result_masks = []
+    result_probs = []
+    results_images = []
+
+    for result in results:
+        if plot:
+            image_bgr = result.plot()
+            results_images.append(image_bgr) #Image RGB
+
+        if mode=='detection' and plot==True:
+            # detection
+            result_boxes.append(result.boxes)
+            #print(result.boxes.xyxy)  # box with xyxy format, (N, 4)
+            #print(result.boxes.xywh)  # box with xywh format, (N, 4)
+            #print(result.boxes.xyxyn)  # box with xyxy format but normalized, (N, 4)
+            #print(result.boxes.xywhn)  # box with xywh format but normalized, (N, 4)
+            #print(result.boxes.conf)  # confidence score, (N, 1)
+            #print(result.boxes.cls)  # cls, (N, 1)
+            return result_boxes, results_images
+        elif mode=='detection' and plot==False:
+            result_boxes.append(result.boxes)
+            return result_boxes
+        
+        elif mode=='segmentation' and plot==True:
+            # segmentation
+            result_masks.append(result.masks)
+            #print(result.masks.masks)     # masks, (N, H, W)
+            #print(result.masks.segments)  # bounding coordinates of masks, List[segment] * N
+            return result_masks, results_images
+        elif mode=='segmentation' and plot==False:
+            result_masks.append(result.masks)
+            return result_masks  
+
+        elif mode=='classification' and plot==True:
+            # classification
+            result_probs.append(result.probs)
+            #print(result.probs)     # cls prob, (num_class, )
+            return result_probs, results_images
+        elif mode=='classification' and plot==False:
+            # classification
+            result_probs.append(result.probs)
+            return result_probs
+        
+        else:
+            print('Wrong mode selected')
