ce0b568f894eb9ed43ed7b029cc6778351ca4ce4
Nicolas Salomon <nicolassalomon96@gmail.com>
2024-10-09 16:21:02 -0300

    Project updated

--- heatmap_video_analysis/main.py
+++ heatmap_video_analysis/main.py
@@ -6,6 +6,7 @@ import cv2
 import torch
 from ultralytics import YOLO
 
+#Object Detector Class
 class Detector():
     def __init__(self, model_path, obj='person'):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -34,10 +35,17 @@ class Detector():
                                      imgsz=704, device=self.device)
         return results
     
+    #Get the center of each bbox detected
     def get_box_center_coor(self, results):
         for r in results:
             centers = [(int(x.item()),int(y.item())) for (x,y,_,_) in(r.boxes.xywh)]
         return centers
+    
+    #Get the corners of each bbox detected
+    def get_box_coor(self, results):
+        for r in results:
+            corners = [(int(x1.item()),int(y1.item()), int(x2.item()), int(y2.item())) for (x1,y1,x2,y2) in(r.boxes.xyxy)]
+        return corners
     
     def draw_center_point(self, image, centers):
         image_frame = np.copy(image)
@@ -45,19 +53,33 @@ class Detector():
             cv2.circle(image_frame, center_coord, 4, (0,0,255), -1)
         return image_frame
 
-
+#Image analyzing and heatmap generator: each time a detection is made, sums 1 to a matrix that records object positions
 class Heatmap_analyzer():
-    def __init__(self, width, height, frames_to_save=200, alpha=0.7, colormap='jet'):
+    def __init__(self, width, height, alpha=0.7):
         self.width = width
         self.height = height
-        self.frames_to_save = frames_to_save
         self.alpha = alpha
-        self.colormap = colormap
-        self.heatmap_matrix = np.zeros((width, height, 3), dtype=np.float32)
+        self.heatmap_matrix = np.zeros((height, width, 3), dtype=np.float32)
     
-    def process(self, frame, centers):
-        pass
-
+    #Only considers the bbox center as object position
+    def process_centers(self, centers):
+        for center in centers:
+            self.heatmap_matrix[center[1], center[0]] += 1
+    
+    #Considers full bbox as object position
+    def process_bbox(self, xyxy):
+        for xyxy_coor in xyxy:
+            self.heatmap_matrix[xyxy_coor[1]:xyxy_coor[3], xyxy_coor[0]:xyxy_coor[2]] += 1
+        
+    def show_and_save(self, last_frame, name='example'):
+        heatmap_blurred = cv2.GaussianBlur(self.heatmap_matrix, (15, 15), 0)
+        heatmap_norm = cv2.normalize(heatmap_blurred, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        heatmap_color = cv2.applyColorMap(heatmap_norm, cv2.COLORMAP_JET)
+        overlay = cv2.addWeighted(last_frame, 1 - self.alpha, heatmap_color, self.alpha, 0)
+    
+        cv2.imshow('Heatmap', overlay)
+        cv2.waitKey(0)
+        cv2.imwrite(rf'outputs\{name}.png', overlay)
 
 if __name__ == '__main__':
 
@@ -67,23 +89,23 @@ if __name__ == '__main__':
     if not os.path.exists(r'.\outputs'):
         os.makedirs(r'.\outputs')
 
-    video_path = r'.\videos\people.mp4'
+    #video_path = r'.\videos\people_loop.mp4'
+    #video_path = r'.\videos\CCTV.mp4'
+    video_path = r'.\videos\Mall.mp4'
     model_path = r'models\yolov8m.pt'
-    #model_path = r'models\yolo11m.pt'
     
-    #Objects initialization 
-    detector = Detector(model_path, obj='car')
-
+    #Video capture and information
     cap = cv2.VideoCapture(video_path)
-    cap.set(3, 1280) #3 -> Width of the frames in the video stream
-    cap.set(4, 720)
-
-    # Codec and VideoWriter
-    codec = cv2.VideoWriter_fourcc(*'MP4V')
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = cap.get(cv2.CAP_PROP_FPS) #10
-    #delay = int(500 / fps)  # delay between frames per miliseconds
-    #video_writer = cv2.VideoWriter(r'.\outputs\output_fail.mp4', codec, fps, (1280,720))
 
+    #Objects initialization 
+    detector = Detector(model_path, obj='person')
+    heatmap = Heatmap_analyzer(width=width, height=height)
+
+    #Processing Loop
+    print("Processing video... This may take some minutes")
     while cap.isOpened():
         
         #Import image from camera
@@ -94,20 +116,32 @@ if __name__ == '__main__':
 
         #Get predictions  
         result = detector.predict(frame)
-        
+
         #Process each frame
         processed_frame = result[0].plot(probs=False, labels=False, boxes=False)
+        
         centers = detector.get_box_center_coor(result) #Get bboxes center
-        centers_frame = detector.draw_center_point(processed_frame, centers)
+        centers_drawn = detector.draw_center_point(processed_frame, centers)
 
-        #Show results        
-        cv2.imshow('processed_frame', centers_frame)
+        xyxy = detector.get_box_coor(result) #Get bboxes corner
+
+        #Heatmap_analysis
+        #heatmap_frame = heatmap.process_centers(centers) #Only considers the bbox center as object position
+        heatmap_frame = heatmap.process_bbox(xyxy) #Considers full bbox as object position
+
+        #Show detections        
+        #cv2.imshow('Processed_frame', centers_drawn)
 
         # Press 'q' to exit the loop
-        #if cv2.waitKey(delay) & 0xFF == ord('q'):
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+    
+    # Get the final frame (only for showing purposes)
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    cap.set(cv2.CAP_PROP_POS_FRAMES, total_frames - 1)
+    _, last_frame = cap.read()
+
+    heatmap.show_and_save(last_frame, name='heatmap_mall')
 
     cap.release()
-    #video_writer.release()
     cv2.destroyAllWindows()
